3fafc2a7cca08b662dd64ede9b5b44cf94511a13
Branch Archer <btaiii@yahoo.com>
2014-12-20 09:15:51 -0600

    Image subtraction implemented

--- canvas.js
+++ canvas.js
@@ -1,27 +1,34 @@
 /* canvasApp for TennisServe.html
 */
 function canvasApp() {
-  var MAX_OFFSET = 10;
+  var MAX_OFFSET = 2;
+  var STEP = 0.1;
   $( "#offsetX" ).spinner( {
     min: -MAX_OFFSET,
     max: MAX_OFFSET,
+    step: STEP,
   // 'change' event only fired after ENTER key pressed; use 'stop' event
     stop: function( event, ui ) {
-      imageToContext(testFrame);
+//      imageToContext(testFrame);
+        performSubtraction();
     }
   } );
   $( "#offsetY" ).spinner( {
     min: -MAX_OFFSET,
     max: MAX_OFFSET,
+    step: STEP,
     stop: function( event, ui ) {
-      imageToContext(testFrame);
+      //imageToContext(testFrame);
+      performSubtraction();
     }
   } );
   $( "#theta" ).spinner( {
     min: -MAX_OFFSET,
     max: MAX_OFFSET,
+    step: 0.01,
     stop: function( event, ui ) {
       imageToContext(testFrame);
+      performSubtraction();
     }
   } );
   $('#alphaSlider').slider( {
@@ -32,40 +39,6 @@ function canvasApp() {
     stop: function( event, ui ) {
       var alpha = $(this).slider('value').toFixed(2);
       $('#alpha').text(alpha);
-      var width = testFrame.canvas.width;
-      var height = testFrame.canvas.height;
-      //imageToContext(testFrame);
-      //var testData = testFrame.context.getImageData(0,0,width, height);
-//      for (i = 3; i < testData.data.length; i += 4) {
-//        testData.data[i] = 255 * alpha;
-//      }
-      testFrame.context.clearRect(0,0,width,height);
-//      testFrame.context.globalAlpha = 1.0;
-//      testFrame.context.drawImage(referenceFrame.image, 0, 0);
-//      testFrame.context.globalAlpha = alpha;
-//      testFrame.context.drawImage(testFrame.image,0,0);
-      //testFrame.context.putImageData(imageData, 0, 0);
-var referenceCanvas = document.createElement('canvas');
-referenceCanvas.width = 640;
-referenceCanvas.height = 480;
-var referenceContext = referenceCanvas.getContext('2d');
-referenceContext.drawImage(referenceFrame.image, 0, 0 );
-var referenceData = referenceContext.getImageData(0, 0, referenceCanvas.width, referenceCanvas.height);
-
-var testCanvas = document.createElement('canvas');
-testCanvas.width = 640;
-testCanvas.height = 480;
-var testContext = testCanvas.getContext('2d');
-testContext.drawImage(testFrame.image, 0, 0 );
-var testData = testContext.getImageData(0, 0, testCanvas.width, testCanvas.height);
-
-Debugger.log(testData.data.length);
-for (var i = 0; i < testData.data.length; i++) {
-  if ((i+1) % 4 == 0) continue;
-  //if (i < 30) Debugger.log(i);
-  testData.data[i] = Math.max(0, testData.data[i] - referenceData.data[i]);
-}
-testFrame.context.putImageData(testData, 0, 0);
     }
   });
 
@@ -79,7 +52,7 @@ testFrame.context.putImageData(testData, 0, 0);
   $(testFrame.image).load( function() {
     imageToContext(testFrame);
   });
-  imageSrc(testFrame, 2);
+  imageSrc(testFrame, 4);
   var jpgNum = /(\d)(\.jpg)/
   $('#next').click( nextImage );
 
@@ -119,6 +92,36 @@ testFrame.context.putImageData(testData, 0, 0);
     frame.image.src = 'Serve-' + digit + '.jpg';
   }
 
+  function performSubtraction() {
+    var offsetX = $('#offsetX').spinner('value');
+    var offsetY = $('#offsetY').spinner('value');
+    var theta = $('#theta').spinner('value');
+    var width = testFrame.canvas.width;
+    var height = testFrame.canvas.height;
+    testFrame.context.clearRect(0,0,width,height);
+    var referenceCanvas = document.createElement('canvas');
+    referenceCanvas.width = 640;
+    referenceCanvas.height = 480;
+    var referenceContext = referenceCanvas.getContext('2d');
+    referenceContext.drawImage(referenceFrame.image, 0, 0 );
+    var referenceData = referenceContext.getImageData(0, 0, referenceCanvas.width, referenceCanvas.height);
+    
+    var testCanvas = document.createElement('canvas');
+    testCanvas.width = 640;
+    testCanvas.height = 480;
+    var testContext = testCanvas.getContext('2d');
+    testContext.translate(offsetX, offsetY);
+    testContext.rotate(theta * Math.PI / 180.0);
+    testContext.drawImage(testFrame.image, 0, 0 );
+    var testData = testContext.getImageData(0, 0, testCanvas.width, testCanvas.height);
+    
+    Debugger.log(testData.data.length);
+    for (var i = 0; i < testData.data.length; i++) {
+      if ((i+1) % 4 == 0) continue;
+      testData.data[i] = 127 + 0.5 * Math.max(0, testData.data[i] - referenceData.data[i]);
+    }
+    testFrame.context.putImageData(testData, 0, 0);
+}
 // http://stackoverflow.com/questions/11444401/perfecting-canvas-mouse-coordinates
   function getPosition(element) {
       var _x = element.offsetLeft;
